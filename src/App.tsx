65e8594e4908cf640c2ce9517806ff1efd2d3bd7
Jens Hincke Friis <jef003@edu.zealand.dk>
2026-08-25 12:15:22 +0200

    Test om Supabase er forbundet til projectet

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import {supabase} from './lib/supabase'
 import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
@@ -6,6 +7,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  console.log('Supabase client:', supabase) // Log the Supabase client to verify it's created successfully
 
   return (
     <>
