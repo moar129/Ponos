a3e3b03a38dfc60571ca7ad66e235a3deadca2aa
Jens Hincke Friis <jef003@edu.zealand.dk>
2026-08-31 13:43:48 +0200

    refactor: remove unused imports and state from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import { Header } from './components/headerComponent'
 import { Footer } from './components/footerComponent'
 import SignUp from './pages/logIn/SignUp'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="min-h-screen flex flex-col justify-between bg-[#F3F4F8]">
       {/* HEADER */}
